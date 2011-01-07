1b6104b937e70deaa5a712c96b05eee1ce28caf0
Mariano Barrios <marianobarrios@gmail.com>
2011-01-07 01:24:22 -0300

    Added packaging

--- src/ygrep/__init__.py
+++
@@ -1,4 +0,0 @@
-# TODO: Add support file names
-# TODO: Add support for multiple streams
-# TODO: -i option
-# TODO: -v option
--- src/ygrep/tests/ygreptest.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python
-from ygrep.ygreplib import Ygrep
-
-file = open('example15.yaml').read()
-print Ygrep(r'\n*', color=True).do_it(file)
-
